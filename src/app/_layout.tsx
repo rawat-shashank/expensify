acc52af8e09efb6a2a1e9ecd58d35f56e7098000
rawat-shashank <shashank17nov@gmail.com>
2025-06-05 19:35:21 +0100

    added material you theme colors, will add support later

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,56 +1,39 @@
 import { Stack } from "expo-router";
 import { SQLiteProvider } from "expo-sqlite";
-import { initializeDatabase } from "@/database";
+import { initializeDatabase, resetDatabase } from "@/database";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import colors from "@/constants/colors";
-import { Platform, SafeAreaView } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { materialTheme } from "@/constants";
+import { useColorScheme } from "react-native";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/queries/queryClient";
+//import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
 
 export default function RootLayout() {
-  const insets = useSafeAreaInsets();
-  if (Platform.OS == "web") {
-    return (
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <QueryClientProvider client={queryClient}>
-          <SQLiteProvider
-            databaseName="expensify.db"
-            // @NOTE: this assetSource databse is read only
-            //assetSource={{ assetId: require("../assets/expensify.db") }}
-            onInit={initializeDatabase}
-          >
-            <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />;
-            </Stack>
-          </SQLiteProvider>
-        </QueryClientProvider>
-      </GestureHandlerRootView>
-    );
-  }
+  //const colorScheme = useColorScheme();
+  // This hook provides the Material You colors.
+  // On Android 12+, it will use the dynamic system colors.
+  // On other platforms/versions, it provides a fallback Material 3 palette.
+  //const { theme } = useMaterial3Theme({ fallbackSourceColor: "#3E8260" });
+
+  // Determine the active theme based on the system's color scheme
+  //const activeColors = theme[colorScheme];
+
+  // TODO: add custom theme
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: colors.bg,
-        paddingTop: insets.top,
-        paddingBottom: insets.bottom,
-      }}
-    >
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <QueryClientProvider client={queryClient}>
-          <SQLiteProvider
-            databaseName="expensify.db"
-            // @NOTE: this assetSource databse is read only
-            //assetSource={{ assetId: require("../assets/expensify.db") }}
-            onInit={initializeDatabase}
-          >
-            <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />;
-            </Stack>
-          </SQLiteProvider>
-        </QueryClientProvider>
-      </GestureHandlerRootView>
-    </SafeAreaView>
+    <GestureHandlerRootView>
+      <QueryClientProvider client={queryClient}>
+        <SQLiteProvider
+          databaseName="expensify.db"
+          // @NOTE: this assetSource databse is read only
+          //assetSource={{ assetId: require("../assets/expensify.db") }}
+          onInit={initializeDatabase}
+        >
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />;
+            <Stack.Screen name="(screens)" options={{ headerShown: false }} />
+          </Stack>
+        </SQLiteProvider>
+      </QueryClientProvider>
+    </GestureHandlerRootView>
   );
 }
